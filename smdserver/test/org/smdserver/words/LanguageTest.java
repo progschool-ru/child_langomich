0ed4262bb8001b69aa4e42ae21f20b8a5662f9af
chivorotkiv@omich.net <chivorotkiv@omich.net>
2011-05-05 16:51:07 +0000

    [server-11] Оказалось что прошлые тесты я не добавил. Но написал тесты на классы пакета words, кроме самих экшнов. Их мы будем тестировать немного по-другому.

---
+++ smdserver/test/org/smdserver/words/LanguageTest.java
@@ -0,0 +1,41 @@
+package org.smdserver.words;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LanguageTest
+{
+	@Test
+	public void testParseJSONSuccess() throws ParseException, JSONException
+	{
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss Z");
+		Word.setDateFormat(format);
+
+		String json = "{name:\"en\",words:[" +
+				"{original:\"первый\",translation:\"first\",rating:4," +
+				"modified:\"2010-10-01 10:00:23 +0600\"}," +
+				"{original:\"второй\",translation:\"second\",rating:0," +
+				"modified:\"2011-10-01 10:10:23 +0600\"}" +
+				"]}";
+
+		Language language = new Language(new JSONObject(json));
+
+		assertEquals("language name", "en", language.getName());
+		assertEquals("words count", 2, language.getWords().size());
+
+		Word first = language.getWords().get(0);
+		Word second = language.getWords().get(1);
+
+		assertEquals("original", "первый", first.getOriginal());
+		assertEquals("translation", "first", first.getTranslation());
+		assertEquals("rating", new Double(4), new Double(first.getRating()));
+
+		assertEquals("original", "второй", second.getOriginal());
+		assertEquals("translation", "second", second.getTranslation());
+		assertEquals("rating", new Double(0), new Double(second.getRating()));
+	}
+}
---
+++ smdserver/test/org/smdserver/words/WordTest.java
@@ -0,0 +1,39 @@
+package org.smdserver.words;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class WordTest
+{
+	@Test
+	public void testParseJSONSuccess() throws JSONException, ParseException
+	{
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeZone(TimeZone.getTimeZone("GMT+6"));
+		calendar.set(Calendar.YEAR, 2010);
+		calendar.set(Calendar.MONTH, Calendar.OCTOBER);
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		calendar.set(Calendar.HOUR_OF_DAY, 10);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 23);
+		Date date = calendar.getTime();
+
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss Z");
+		Word.setDateFormat(format);
+
+		String json = "{original:\"первый\",translation:\"first\",rating:4,modified:\"2010-10-01 10:00:23 +0600\"}";
+		Word word = new Word(new JSONObject(json));
+
+		assertEquals("original", "первый", word.getOriginal());
+		assertEquals("translation", "first", word.getTranslation());
+		assertEquals("rating", new Double(4), new Double(word.getRating()));
+		assertEquals("modified", format.format(date), format.format(word.getModified()));
+	}
+}
