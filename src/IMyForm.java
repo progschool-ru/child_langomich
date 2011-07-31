53bf6cb91007a495509d6ba3e1d40e6ed12b4fc6
Skripnikov.Sergey@gmail.com <Skripnikov.Sergey@gmail.com>
2011-07-31 16:33:09 +0000

    Создал еще 2 формы. MainForm - главное меню. ListForm - словарь. А также их общего родителя myForm. Внес много изменений связанных с прорисовкой. Формы в которых использовался высокоуровневый интерфейс упали, поэтому пока можно пользоваться лишь новыми.

---
+++ src/IMyForm.java
@@ -0,0 +1,18 @@
+interface IMyForm
+{
+    public String[] getList();
+    public String[] getPaths();
+    public void drawSomething();
+    public String getMainPath();
+    public String getMainName();
+    public void mainNumber();
+    public void up();
+    public void down();
+    public void forward();
+    public void back();
+    public void dieIsCast();
+    public int getSelectedRowTopY();
+    public int getSelectedRowHeight();
+    public int getMainSelectedRowTopY();
+    public int getMainSelectedRowHeight();
+}
--- src/SmartDictionary.java
+++ src/SmartDictionary.java
@@ -6,52 +6,43 @@ public class SmartDictionary extends MIDlet implements CommandListener
         private Text text = new Text();
 
 	private Command exitMIDlet = new Command(text.EXIT, Command.EXIT, 0);
-	private Command choice = new Command(text.CHOICE, Command.SCREEN, 1);
 	private Command back = new Command(text.BACK, Command.EXIT, 0);
         private Command backF2 = new Command(text.BACK, Command.EXIT, 0);
 	private Command OK = new Command(text.OK, Command.SCREEN, 1);
 
         private Command next = new Command(text.NEXT, Command.SCREEN, 1);
         private Command Save = new Command(text.SAVE, Command.SCREEN, 1);
-        private Command delete = new Command(text.DELETE, Command.SCREEN, 1);
-        private Command ordering1 = new Command(text.ORDERING_1, Command.SCREEN, 1);
-        private Command ordering2 = new Command(text.ORDERING_2, Command.SCREEN, 1);
 
-	private List mainList;
-        private Form workForm = new Form(text.START);
-        private Form addWordForm = new Form(text.ADD_WORD);
-	private List dictionaryList;
+        public Form workForm = new Form(text.START);
+        public Form addWordForm = new Form(text.ADD_WORD);
 
         private String[] cgName = {text.DONT_KNOW, text.BAD, text.NORMALLY, text.GOOD, text.VERY_GOOD};
         private TextField tfRus = new TextField(text.ENTER_THE_WORD_ORIGINALLY, "", 20, TextField.ANY);
         private TextField tfEng = new TextField(text.ENTER_TRANSLATION, "", 20, TextField.ANY);
         private ChoiceGroup mycg = new ChoiceGroup(text.KNOWLEDGE, ChoiceGroup.POPUP, cgName, null);
 
-        private String[] name = {text.START, text.ADD_WORD, text.DICTIONARY, text.SETTINGS};
-	private TextField[] tf;
+      	private TextField[] tf;
         private StringItem[] siAnswer;
         private StringItem si = new StringItem(text.EMPTY_DICTIONARY,"");
 
         private Dictionary dictionary;
-        private Languages languages;
         private Settings settings = new Settings();
 
         private int rows[];
-	private int P = 1;
         private int wordsN = 1;
+
+        MainForm mf;
 
 	public void startApp() 
 	{
-                if(settings.getLanguage().equals("null"))
-                    dictionary = new Dictionary();
-                dictionary = new Dictionary(settings.getLanguage());
-                languages = new Languages();
+             if(settings.getLanguage().equals("null"))
+                dictionary = new Dictionary();
+            dictionary = new Dictionary(settings.getLanguage());
 
-		mainListInit();
-                    workFormInit();
-                    addWordFormInit();
+            workFormInit();
+            addWordFormInit();
 
-                Display.getDisplay(this).setCurrent(mainList);
+            goToTheMainForm();
 	}
 	public void pauseApp() {}
 
@@ -61,18 +52,16 @@ public class SmartDictionary extends MIDlet implements CommandListener
 	{
 		if(c == exitMIDlet)
 		{
-			dictionary.destroy();
-                        languages.destroy();
 			destroyApp(false);
 			notifyDestroyed();
 		}
 		if(c == back) 
 		{
-			Display.getDisplay(this).setCurrent(mainList);
+			Display.getDisplay(this).setCurrent(mf);
 		}
 		if(c == backF2)
 		{            
-                    Display.getDisplay(this).setCurrent(mainList);
+                    Display.getDisplay(this).setCurrent(mf);
 		}
 		if(c == OK) 
 		{
@@ -89,73 +78,16 @@ public class SmartDictionary extends MIDlet implements CommandListener
                 if(c == Save)
 		{
                         dictionary.newRecord(tfRus.getString(), tfEng.getString(), mycg.getSelectedIndex()*2);
-			Display.getDisplay(this).setCurrent(mainList);
+			Display.getDisplay(this).setCurrent(mf);
 		}
-		if (c == choice)
-		{
-			int i = mainList.getSelectedIndex();
-			if(i == 0)
-			{
-                                workFormReset();
-				Display.getDisplay(this).setCurrent(workForm);
-			}
-			if (i == 1)
-			{       
-                                if(settings.getLanguage().equals("null"))
-                                {
-
-                                }
-                                else
-                                {
-                                    addWordFormReset();
-                                    Display.getDisplay(this).setCurrent(addWordForm);
-                                }
-			}
-			if (i == 2)
-			{
-				dictionaryListInit();
-				Display.getDisplay(this).setCurrent(dictionaryList);
-			}
-                        if (i == 3)
-                            goToTheSettingsForm();
-		}
-                if (c == delete) 
-		{
-                        dictionary.deleteRecord(dictionaryList.getSelectedIndex()+1);
-			dictionaryList.delete(dictionaryList.getSelectedIndex());
-                        if(dictionary.getNumRecords() == 0)
-                            dictionaryListInit();
-			Display.getDisplay(this).setCurrent(dictionaryList);
-                }
-                if (c == ordering1)
-		{
-                    P = 1;
-                    dictionary.newOrdering(P);
-                    dictionaryListInit();
-                    Display.getDisplay(this).setCurrent(dictionaryList);
-                }
-                if (c == ordering2)
-		{
-                    P = 2;
-                    dictionary.newOrdering(P);
-                    dictionaryListInit();
-                    Display.getDisplay(this).setCurrent(dictionaryList);
-                }
 	}
-	private void mainListInit()
-	{
-		mainList = new List(text.MENU, Choice.IMPLICIT, name, null);
-		mainList.addCommand(choice);
-		mainList.addCommand(exitMIDlet);
-		mainList.setCommandListener(this);
-	}
-        private void workFormInit()
+        public void workFormInit()
 	{
 		workForm.addCommand(OK);
 		workForm.addCommand(back);
 		workForm.setCommandListener(this);
 	}
- 	private void workFormReset()
+ 	public void workFormReset()
 	{
             workForm.removeCommand(OK);
             workForm.removeCommand(next);
@@ -189,7 +121,7 @@ public class SmartDictionary extends MIDlet implements CommandListener
                 }
             }
 	}
-        private void workFormAnswer(boolean answer[])
+        public void workFormAnswer(boolean answer[])
         {
             workForm.removeCommand(OK);
             workForm.addCommand(next);
@@ -204,13 +136,13 @@ public class SmartDictionary extends MIDlet implements CommandListener
                 workForm.append(siAnswer[i]);
             }
         }
-	private void addWordFormInit()
+	public void addWordFormInit()
         {
                 addWordForm.addCommand(back);
                 addWordForm.addCommand(Save);
 		addWordForm.setCommandListener(this);
 	}
-	private void addWordFormReset()
+	public void addWordFormReset()
 	{
 		mycg = new ChoiceGroup(text.KNOWLEDGE, ChoiceGroup.POPUP, cgName, null);
                 tfRus.delete(0, tfRus.getString().length());
@@ -220,27 +152,9 @@ public class SmartDictionary extends MIDlet implements CommandListener
                 addWordForm.append(tfEng);
 		addWordForm.append(mycg);
 	}
-        private void dictionaryListInit()
-	{
-                if(dictionary.getNumRecords() == 0)
-                {
-                        String emptyDictionary[] = new String[1];
-                        emptyDictionary[0] = text.EMPTY_DICTIONARY;
-                        dictionaryList = new List(text.DICTIONARY, Choice.IMPLICIT, emptyDictionary, null);
-                }
-                else
-                {
-                    dictionaryList = new List(text.DICTIONARY, Choice.IMPLICIT, dictionary.getFullRecords(), null);
-                    dictionaryList.addCommand(delete);
-                    dictionaryList.addCommand(ordering1);
-                    dictionaryList.addCommand(ordering2);     
-                }
-                dictionaryList.addCommand(back);
-                dictionaryList.setCommandListener(this);
-        }
-        private void goToTheSettingsForm()
+        private void goToTheMainForm()
         {
-            SettingsForm sf = new SettingsForm(Display.getDisplay(this), mainList);
-            Display.getDisplay(this).setCurrent(sf);
+            mf = new MainForm(this);
+            Display.getDisplay(this).setCurrent(mf);
         }
 }
