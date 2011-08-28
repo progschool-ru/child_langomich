9f35525afcb92275fdb95bd882440c28cbd5e500
chivorotkiv@omich.net <chivorotkiv@omich.net>
2011-08-28 22:26:10 +0000

    [server-#26601] Maintenance экшны отаставали от реального положения дел. Поправил.

--- smdserver/src/java/org/smdserver/maintenance/DeployDBTablesAction.java
+++ smdserver/src/java/org/smdserver/maintenance/DeployDBTablesAction.java
@@ -18,6 +18,7 @@ public class DeployDBTablesAction extends DBAction
 					"psw VARCHAR (32) NOT NULL," +
 					"is_blocked BOOLEAN NOT NULL DEFAULT '1'," +
 					"email VARCHAR (80) NOT NULL," +
+					"about VARCHAR (254) NOT NULL," +
 					"time_created DATETIME NOT NULL," +
 					"time_modified DATETIME NOT NULL," +
 					"PRIMARY KEY (user_id)" +
@@ -48,13 +49,28 @@ public class DeployDBTablesAction extends DBAction
 				"PRIMARY KEY (language_id, original)," +
 				"FOREIGN KEY (language_id) REFERENCES %1$slanguages(language_id) ON DELETE CASCADE," +
 				"INDEX(translation)" +
-			")  ENGINE = InnoDB CHARACTER SET = utf8;"
+			")  ENGINE = InnoDB CHARACTER SET = utf8;",
+			
+			"CREATE TABLE %1$sregistration_requests" +
+				"(" +
+					"user_id VARCHAR (36) NOT NULL," +
+					"login_key VARCHAR (80) UNIQUE NOT NULL," +
+					"login VARCHAR (80) NOT NULL," +
+					"psw VARCHAR (32) NOT NULL," +
+					"is_blocked BOOLEAN NOT NULL DEFAULT '1'," +
+					"email VARCHAR (80) NOT NULL," +
+					"about VARCHAR (254) NOT NULL," +
+					"time_created DATETIME NOT NULL," +
+					"time_modified DATETIME NOT NULL," +
+					"PRIMARY KEY (user_id)" +
+				") ENGINE = InnoDB CHARACTER SET = utf8;"
 		};
 		
 		List<String> queries = new ArrayList<String>();
 		queries.add(String.format(templates[0], tablesPrefix));
 		queries.add(String.format(templates[1], tablesPrefix));
 		queries.add(String.format(templates[2], tablesPrefix));
+		queries.add(String.format(templates[3], tablesPrefix));
 
 		return queries;
 	}	
--- smdserver/src/java/org/smdserver/maintenance/DropDBTablesAction.java
+++ smdserver/src/java/org/smdserver/maintenance/DropDBTablesAction.java
@@ -14,13 +14,15 @@ public class DropDBTablesAction extends DBAction
 		String [] templates = {
 			"DROP TABLE %1$swords;",
 			"DROP TABLE %1$slanguages;",
-			"DROP TABLE %1$susers;"
+			"DROP TABLE %1$susers;",
+			"DROP TABLE %1$sregistration_requests"
 		};
 		
 		List<String> queries = new ArrayList<String>();
 		queries.add(String.format(templates[0], tablesPrefix));
 		queries.add(String.format(templates[1], tablesPrefix));
 		queries.add(String.format(templates[2], tablesPrefix));
+		queries.add(String.format(templates[3], tablesPrefix));
 		
 		return queries;
 	}	
