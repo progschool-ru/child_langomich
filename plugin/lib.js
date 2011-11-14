331027cbe492873f33359fc76a13310455fc53e2
archerigoli1@gmail.com <archerigoli1@gmail.com>
2011-11-14 18:16:54 +0000

    Добавил класс SMDServer который возьмет на себя все операции с сервером

---
+++ plugin/lib.js
@@ -0,0 +1,53 @@
+﻿function sendAlert(text){
+	chrome.tabs.executeScript(null, sAlert(text));
+}
+
+function sAlert(text){
+	alert(text);
+}
+
+//Убираем символы экранирования
+//Скорее всего тоже надо переделать
+function copyStr(str){
+	var NewStr = "";
+		for(var i=0; i<str.length; i++){
+			if(str.charAt(i) != "\\") NewStr = NewStr+str.charAt(i);
+		}
+	return NewStr;
+}
+
+var SMDServer = {
+	login: "",
+	password: "",
+	
+	setAuthData: function(Login, Password){
+		login = Login;
+		password = Password;
+	},
+	
+	auth : function(handler){
+		var authData = "login="+login+"&password="+password;
+		 $.ajax({
+			url: "http://lang.omich.net/smdserver/servlet/login",
+			type: "POST",
+			data: authData,
+			success: function(data){ 
+						var аuthdata = JSON.parse(copyStr(data));
+						handler(аuthdata.success);
+					},
+			error: function(){ sendAlert("error");} 
+		});
+	},
+	
+	LoadLanguages : function(handler){
+		$.ajax({
+			url: "http://lang.omich.net/smdserver/servlet/getLanguages",
+			success: function(data){ 
+						var res = JSON.parse(copyStr(data));
+						handler(res);
+					},
+			error: function(){ sendAlert("error");} 
+		});
+		
+	}
+}
