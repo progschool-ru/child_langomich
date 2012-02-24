7b58c4b4f89340da8cc9c15cfde85da224262ee9
archerigoli1@gmail.com <archerigoli1@gmail.com>
2012-02-24 18:24:24 +0000

    up

---
+++ Android/LangOmich/src/org/omich/lang/SQLite/LanguagesData.java
@@ -0,0 +1,73 @@
+package org.omich.lang.SQLite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.omich.lang.words.Language;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class LanguagesData {
+	
+	private String[] allLanguageColoms = { MySQLiteHelper.LANGUAGE_ID, MySQLiteHelper.NAME };
+	
+	SQLiteDatabase database;
+	MySQLiteHelper dbHelper;
+	
+	public LanguagesData(Context context) {
+		dbHelper = new MySQLiteHelper(context);
+	}
+	
+	public void open(){
+		database = dbHelper.getWritableDatabase();
+	}
+	
+	public void close(){
+		database.close();
+	}
+	public void createLanguage(Language language){
+		ContentValues value = new ContentValues();
+		
+		value.put(MySQLiteHelper.LANGUAGE_ID, language.getId());
+		value.put(MySQLiteHelper.NAME, language.getName());
+
+		database.insert(MySQLiteHelper.LANGUAGES_TABLE, null, value);
+	}
+	
+	public List<Language> getAllLanguages(){
+		
+		List<Language> languages = new ArrayList<Language>();
+		
+		Cursor cursor = database.query(MySQLiteHelper.LANGUAGES_TABLE, 
+				allLanguageColoms, null, null, null, null, null);
+		
+		cursor.moveToFirst();
+		
+		while(cursor.isAfterLast()){
+			Language language = cusorToLanguage(cursor);
+			languages.add(language);
+			cursor.moveToNext();
+		}
+		
+		return languages;
+	}
+	
+	public void deleteLanguage(Language language){
+		String language_id = language.getId();
+		
+		database.delete(MySQLiteHelper.LANGUAGES_TABLE,
+				MySQLiteHelper.LANGUAGE_ID + " = " + language_id, null);
+	}
+	
+	private Language cusorToLanguage(Cursor cursor){
+		
+		String languageId = cursor.getString(0);
+		String name = cursor.getString(1);
+		
+		return new Language(name, languageId, null);
+	}
+	
+}
---
+++ Android/LangOmich/src/org/omich/lang/SQLite/WordsData.java
@@ -0,0 +1,87 @@
+package org.omich.lang.SQLite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.omich.lang.words.Language;
+import org.omich.lang.words.Word;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+public class WordsData {
+	
+	private SQLiteDatabase database;
+	private MySQLiteHelper dbHelper;
+	
+	private Language language;
+	
+	private String[] wordColoms = {MySQLiteHelper.ORIGINAL, MySQLiteHelper.TRANSLATION, MySQLiteHelper.RATING, MySQLiteHelper.MODIFIED };
+	
+	public WordsData(Context context, Language language){
+		dbHelper = new MySQLiteHelper(context);
+		
+	}
+	
+	public void open() throws SQLException{
+		database = dbHelper.getWritableDatabase();
+	}
+	
+	public void close(){
+		dbHelper.close();
+	}
+	
+	public void setLanguage(Language language){
+		this.language = language;
+	}
+	
+	public void createWord(Word word){
+		ContentValues value = new ContentValues();
+		value.put(MySQLiteHelper.ORIGINAL, word.getOriginal());
+		value.put(MySQLiteHelper.TRANSLATION, word.getTranslation());
+		value.put(MySQLiteHelper.LANGUAGE_ID, language.getId());
+		value.put(MySQLiteHelper.RATING, word.getRating());
+		value.put(MySQLiteHelper.MODIFIED,word.getModified());
+		
+		database.insert(MySQLiteHelper.WORDS_TABLE, null, value);
+	}
+	
+	public List<Word> getAllWords(){
+		
+		List<Word> words = new ArrayList<Word>();
+		
+		Cursor cursor = database.query(MySQLiteHelper.WORDS_TABLE,
+						wordColoms, MySQLiteHelper.LANGUAGE_ID + " = " + language.getId(),
+						null, null, null, null);
+		
+		cursor.moveToFirst();
+		
+		while(cursor.isAfterLast()){
+			Word word = cursortoWord(cursor);
+			words.add(word);
+			cursor.moveToNext();
+		}
+		
+		return words;
+	}
+	
+	public void deleteWord(Word word){
+		
+		database.delete(MySQLiteHelper.WORDS_TABLE,
+				MySQLiteHelper.ORIGINAL+"="+word.getOriginal()+" and "+MySQLiteHelper.TRANSLATION +" = "+ word.getTranslation(),
+				null);
+	}
+	
+	private Word cursortoWord(Cursor cursor){
+		String original = cursor.getString(0);
+		String translation = cursor.getString(1);
+		int rating = cursor.getInt(2);
+		long modified = cursor.getLong(3);
+		
+		return new Word(original,translation,rating,modified);
+	}
+	
+}
