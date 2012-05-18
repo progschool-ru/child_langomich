d7fbfc0f9812de94f7adacf8420d35e0c459e5a3
chivorotkiv@omich.net <chivorotkiv@omich.net>
2012-05-18 17:02:16 +0000

    [web-#70193] Скриптом создаём список вкладок

--- smdserver/web/js/org.omich.lang/App.js
+++ smdserver/web/js/org.omich.lang/App.js
@@ -2,11 +2,30 @@
 {
 	var ns = org.omich.nsSelf("lang");
 	
+	ns.AuthTypes = {
+		AUTH: 0,
+		NO_AUTH: 1,
+		DOESNT_MATTER: 2
+	};
+	
 	ns.App = org.omich.Class.extend({
 		init: function ()
 		{
 			var $tabsPanel = $("#org-omich-lang-tabsPanel");
-			this._modTabsPanel = new ns.ModuleTabsPanel($tabsPanel);
+			this._modTabsPanel = new ns.ModuleTabsPanel($tabsPanel, {tabs:[{
+					title: "Login",
+					pageId: "login",
+					authType: ns.AuthTypes.NO_AUTH
+				},{
+					title: "Register",
+					pageId: "register",
+					authType: ns.AuthTypes.NO_AUTH
+				},{
+					title: "About",
+					pageId: "about",
+					authType: ns.AuthTypes.DOESNT_MATTER
+				}
+			]});
 		}
 	});
 	
--- smdserver/web/js/org.omich.lang/ModuleTabsPanel.js
+++ smdserver/web/js/org.omich.lang/ModuleTabsPanel.js
@@ -1,11 +1,29 @@
 (function ()
 {
 	var ns = org.omich.nsSelf("lang");
+
+	var initTabs = function (tabs, $div)
+	{
+		var $ul = $("<ul/>");
+		$ul.addClass("header-menu");
+		for(var i = 0; i < tabs.length; ++i)
+		{
+			var $li = $("<li/>");
+			var $a = $("<a/>");
+			$a.attr("href", "#page/" + tabs[i].pageId);
+			$a.append(tabs[i].title);
+			$li.append($a);
+			$ul.append($li);
+		}
+		
+		$div.append($ul);
+	}
 	
 	ns.ModuleTabsPanel = org.omich.Class.extend({
-		init: function ($div)
+		init: function ($div, settings)
 		{
-			$div.append("Success");
-		}
+			this._$div = $div;
+			initTabs(settings.tabs, $div);
+		},
 	});
 })();
