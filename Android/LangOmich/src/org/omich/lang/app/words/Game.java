bd194b3fbdc0648e306a774f67df894f6686d4e2
Skripnikov.Sergey@gmail.com <Skripnikov.Sergey@gmail.com>
2012-07-28 12:46:34 +0000

    Разделил класс GameActivity на два, создав класс Game. В GameActivity по большей части осталась лишь работа интерфейсом. Все остальное было помещено в класс Game.

---
+++ Android/LangOmich/src/org/omich/lang/app/words/Game.java
@@ -0,0 +1,164 @@
+package org.omich.lang.app.words;
+
+import java.util.ArrayList;
+
+import org.omich.lang.R;
+import org.omich.lang.app.BundleFields;
+import org.omich.lang.app.db.Word;
+import org.omich.tool.bcops.BcConnector;
+import org.omich.tool.events.Listeners.IListener;
+import org.omich.tool.events.Listeners.IListenerVoid;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Game 
+{
+	private BcConnector mBcConnector;
+
+	private long dictId;
+	private int idealNumber = 3;
+	private int realNumber = 0;
+	private int currentNumber = 0;
+	private Word word;
+	private ArrayList<Word> words;
+	
+	private String dictIsEmptyNativ;
+	private String dictIsEmptyEnglish;
+	
+	private String mSetRatingTaskId;
+	private String mGetRandomWordsTaskId;	
+	private boolean mIsDestroyed;
+	
+	public final int UPGRADE = 1;
+	public final int DOWNGRADE = 2;
+	
+	public final int NATIV = 1;
+	public final int FOREIGN = 2;	
+	
+	private IListenerVoid lv;
+			
+	public Game(Context context, long dictId, IListenerVoid lv)
+	{
+		this.lv = lv;
+		mBcConnector = new BcConnector(context);
+		dictIsEmptyNativ = context.getResources().getString(R.string.game_die_nativ);
+		dictIsEmptyEnglish = context.getResources().getString(R.string.game_die_english);
+		setNewDictId(dictId);
+	}
+	public void setNewDictId(long dictId)
+	{
+		this.dictId = dictId;
+		getRandomWords();
+	}
+	public String getText(int key)
+	{
+		String text = "";
+		if(word != null)
+			switch (key)
+			{
+				case NATIV:				
+					text = word.nativ;
+					break;
+				case FOREIGN:
+					text = word.foreign;
+					break;
+			}
+		return text;
+	}
+	public void changeTheRating(int key)
+	{
+		int rating = 0;
+		switch (key)
+		{
+			case UPGRADE:				
+				if(word.rating<9)
+					rating = word.rating + 1;
+				else
+					rating = 9;
+				break;
+		    case DOWNGRADE:
+				if(word.rating>2)
+					rating = word.rating - 2;
+				else
+					rating = 0;
+				break;
+		}
+		setNewRating(word.id, rating);
+	}
+	public void getNextWord()
+	{
+		if(words == null || currentNumber == realNumber)
+			getRandomWords();
+		else
+			updateWord();
+	}	
+	private void getRandomWords()
+	{
+		if(mGetRandomWordsTaskId != null)
+			return;		
+		
+		Intent intent = GetRandomWordsTask.createIntent(dictId, idealNumber);
+		mGetRandomWordsTaskId = mBcConnector.startTypicalTask(GetRandomWordsTask.class, intent, new IListener<Bundle>()
+						{
+							public void handle (Bundle b)
+							{
+								if(mIsDestroyed)
+									return;
+								
+								mGetRandomWordsTaskId = null;
+			
+								words = b.<Word>getParcelableArrayList(BundleFields.WORDS_LIST);	
+								realNumber = words.size();
+								currentNumber = 0;
+								
+								updateWord();
+							}
+						});
+	}	
+	private void updateWord()
+	{
+		if(realNumber > 0)
+		{
+			word = words.get(currentNumber);
+			currentNumber++;
+		}
+		else
+			word = new Word(dictIsEmptyNativ, dictIsEmptyEnglish, 0, -1); 
+		lv.handle();
+	}	
+	private void setNewRating(long id, int rating)
+	{
+		if(mSetRatingTaskId != null)
+			return;
+
+		Intent intent = SetNewRatingTask.createIntent(id,rating);
+		mSetRatingTaskId = mBcConnector.startTypicalTask(SetNewRatingTask.class, 
+				intent, 
+				new IListener<Bundle>()
+				{
+					public void handle (Bundle bundle)
+					{
+						if(mIsDestroyed)
+							return;
+				
+						mSetRatingTaskId = null;
+					}
+				});
+	}
+	public void destroy()
+	{		
+		if(mGetRandomWordsTaskId != null)
+		{
+			mBcConnector.unsubscribeTask(mGetRandomWordsTaskId);
+			mGetRandomWordsTaskId = null;
+		}
+		if(mSetRatingTaskId != null)
+		{
+			mBcConnector.unsubscribeTask(mSetRatingTaskId);
+			mSetRatingTaskId = null;
+		}	
+		mIsDestroyed = true;
+	}
+}
--- Android/LangOmich/src/org/omich/lang/app/words/GameActivity.java
+++ Android/LangOmich/src/org/omich/lang/app/words/GameActivity.java
@@ -1,15 +1,9 @@
 package org.omich.lang.app.words;
 
-import java.util.ArrayList;
-
 import org.omich.lang.R;
-import org.omich.lang.app.BundleFields;
 import org.omich.lang.app.PreferenceFields;
-import org.omich.lang.app.db.Word;
 import org.omich.lang.apptool.activity.ABActivity;
-import org.omich.tool.events.Listeners.IListener;
-
-import android.content.Intent;
+import org.omich.tool.events.Listeners.IListenerVoid;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
@@ -29,16 +23,8 @@ public class GameActivity extends ABActivity implements OnSharedPreferenceChange
 	private Button bt1;
 	private Button bt2;
 	private int currentScreen = GAME;
-	
-	private Word word;
-	private String mGetWordTaskId;
-	private String mSetRatingTaskId;
-	private String mGetWordsTaskId;
-	
-	private int process = 0;
-	private int size = 0;
-	ArrayList<Word> words;
 
+	private Game game;
 	//==== live cycle =========================================================
 	@Override
 	protected void onCreate (Bundle b)
@@ -51,6 +37,17 @@ public class GameActivity extends ABActivity implements OnSharedPreferenceChange
 		tvf = (TextView)findViewById(R.id.item_wordslist_text_foreign);
 		bt1 = (Button)findViewById(R.id.button_1);
 		bt2 = (Button)findViewById(R.id.button_2);
+		
+		game = new Game(this,sp.getLong(PreferenceFields.DICT_ID, -1), new IListenerVoid()
+		{
+			public void handle ()
+			{ 
+				tvn.setText(game.getText(game.NATIV));
+				bt1.setVisibility(View.VISIBLE);
+				bt2.setVisibility(View.VISIBLE);			
+			}			
+		});
+		
 		OnClickListener l =  new OnClickListener(){
 			public void onClick(View v) 
 			{
@@ -63,11 +60,7 @@ public class GameActivity extends ABActivity implements OnSharedPreferenceChange
 								setScreen(I_KNOW);
 								break;
 							case I_KNOW:
-								upgradeRating();
-								setScreen(GAME);
-								break;
-							case I_DONT_KNOW:
-								downgradeRating();
+								game.changeTheRating(game.UPGRADE);
 								setScreen(GAME);
 								break;
 						}
@@ -79,16 +72,22 @@ public class GameActivity extends ABActivity implements OnSharedPreferenceChange
 								setScreen(I_DONT_KNOW);
 								break;
 							case I_KNOW:
-								downgradeRating();
+								game.changeTheRating(game.DOWNGRADE);
 								setScreen(GAME);	
 								break;
+							case I_DONT_KNOW:
+								game.changeTheRating(game.DOWNGRADE);
+								setScreen(GAME);
+								break;								
 						}
 						break;
 				}
 			}
 		};
+		
 		bt1.setOnClickListener(l);
 		bt2.setOnClickListener(l);
+		
 		setScreen(GAME);
 	}
 	public void onSharedPreferenceChanged(SharedPreferences prefs, String key) 
@@ -100,26 +99,15 @@ public class GameActivity extends ABActivity implements OnSharedPreferenceChange
 			else
 				itemTiming.setIcon(R.drawable.ic_sunc_disable);			
 		}
+		else if(key.equals(PreferenceFields.DICT_ID))
+		{
+			game.setNewDictId(sp.getLong(PreferenceFields.DICT_ID, -1));
+		}
 	}	
 	@Override
 	protected void onDestroy ()
 	{
-		if(mGetWordTaskId != null)
-		{
-			getBcConnector().unsubscribeTask(mGetWordTaskId);
-			mGetWordTaskId = null;
-		}
-		
-		if(mGetWordsTaskId != null)
-		{
-			getBcConnector().unsubscribeTask(mGetWordsTaskId);
-			mGetWordsTaskId = null;
-		}
-		if(mSetRatingTaskId != null)
-		{
-			getBcConnector().unsubscribeTask(mSetRatingTaskId);
-			mSetRatingTaskId = null;
-		}		
+		game.destroy();
 		super.onDestroy();
 	}
 	
@@ -129,123 +117,27 @@ public class GameActivity extends ABActivity implements OnSharedPreferenceChange
 		if(screen == GAME)
 		{
 			tvn.setText("");
-			tvf.setText("");
-			getWord();
+			tvf.setText("");		
 			bt1.setText(getString(R.string.i_know));
 			bt2.setText(getString(R.string.i_dont_know));
-			bt2.setVisibility(View.VISIBLE);
+			bt1.setVisibility(View.INVISIBLE);
+			bt2.setVisibility(View.INVISIBLE);
+			game.getNextWord();
 			currentScreen = GAME;		
 		}
 		else if(screen == I_KNOW)
 		{
-			tvf.setText(word.foreign);
+			tvf.setText(game.getText(game.FOREIGN));
 			bt1.setText(getString(R.string.isTrue));
 			bt2.setText(getString(R.string.isFalse));
 			currentScreen = I_KNOW;	
 		}
 		else if(screen == I_DONT_KNOW)
 		{
-			tvf.setText(word.foreign);
-			bt1.setText(getString(R.string.next));
-			bt2.setVisibility(View.INVISIBLE);
+			tvf.setText(game.getText(game.FOREIGN));
+			bt1.setVisibility(View.INVISIBLE);
+			bt2.setText(getString(R.string.next));
 			currentScreen = I_DONT_KNOW;
 		}		
-	}
-	private void upgradeRating()
-	{
-		int rating;
-		if(word.rating<9)
-			rating = word.rating + 1;
-		else
-			rating = 9;
-		setNewRating(word.id, rating);
-	}
-
-	private void downgradeRating()
-	{
-		int rating;
-		if(word.rating>2)
-			rating = word.rating - 2;
-		else
-			rating = 0;
-		setNewRating(word.id, rating);
-	}
-
-	private void getWord ()
-	{
-		if(words == null || process == size)
-			getWords();
-		else
-		{
-			word = words.get(process);
-			tvn.setText(word.nativ);
-			process++;	
-		}
-/*		if(mGetWordTaskId != null)
-			return;
-
-		Intent intent = GetRandomWordTask.createIntent(sp.getLong(PreferenceFields.DICT_ID, -1));
-		getBcConnector().startTypicalTask(GetRandomWordTask.class, intent, new IListener<Bundle>()
-						{
-							public void handle (Bundle b)
-							{
-								if(mIsDestroyed)
-									return;
-
-								mGetWordTaskId = null;
-			
-								word = b.getParcelable(BundleFields.WORD);	
-								tvn.setText(word.nativ);
-							}
-						});
-*/
-	}	
-	private void getWords ()
-	{
-		if(mGetWordsTaskId != null)
-			return;
-		int number = 3;
-		Intent intent = GetRandomWordsTask.createIntent(sp.getLong(PreferenceFields.DICT_ID, -1), number);
-		mGetWordsTaskId = getBcConnector().startTypicalTask(GetRandomWordsTask.class, intent, new IListener<Bundle>()
-						{
-							public void handle (Bundle b)
-							{
-								mGetWordsTaskId = null;
-			
-								words = b.<Word>getParcelableArrayList(BundleFields.WORDS_LIST);	
-								process = 0;
-								size = words.size();
-								if(size != 0)
-								{
-									word = words.get(process);
-									tvn.setText(word.nativ);
-									process++;
-								}
-								else
-								{
-									word = new Word("Словарь пуст", "Dictionary is empty", 0, -1);
-									tvn.setText(word.nativ);
-								}
-							}
-						});
-	}		
-	private void setNewRating (long id, int rating)
-	{
-		if(mSetRatingTaskId != null)
-			return;
-
-		Intent intent = SetNewRatingTask.createIntent(id,rating);
-		mSetRatingTaskId = getBcConnector().startTypicalTask(SetNewRatingTask.class, 
-				intent, 
-				new IListener<Bundle>()
-				{
-					public void handle (Bundle bundle)
-					{
-						if(mIsDestroyed)
-							return;
-				
-						mSetRatingTaskId = null;
-					}
-				});
 	}
 }
